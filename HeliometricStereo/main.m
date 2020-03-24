c430c70c547fa2fccf146e13cb15082b7030f31f
Zifeng <huangsnake@gmail.com>
2020-03-24 12:01:01 +0800

    一些小改动

--- HeliometricStereo/main.m
+++ HeliometricStereo/main.m
@@ -1,52 +1,52 @@
-%主脚本，包含了一轮重建的所有步骤
-
-%——————————————————————————————————————
-%基础文件夹指定和文件遍历数量指定
-
-lightsFolder='Resources/Lights';
-objectFolder='Resources/Apollo';
-endIdx=6;
-
-%——————————————————————————————————————
-%计算光源信息
-
-disp('开始光源处理');
-% %用标定球计算光源
-% lightPos=calLight(lightsFolder,endIdx);
-%用地理信息计算光源
-lightVec=calLightSun(lightsFolder,endIdx);
-%保存光源
-save([lightsFolder,'/lightPos.txt'],'lightVec','-ascii');
-%读取光源
-lightVec = load([lightsFolder,'/lightPos.txt'],'-ascii');
-disp('光源处理完毕');
-
-%——————————————————————————————————————
-%还原表面法线并显示
-
-%计算法线和反射率rho
-disp('开始计算法线和反射率');
-[normals,rho]=calNormals(objectFolder,endIdx,lightVec);
-disp('计算法线和反射率完毕');
-
-%显示法线图
-show_surfNorm(normals, 4);
-
-%RGB显示法线图
-figure;
-imshow(toNormalImg(normals));
-
-%显示反射率图
-figure;
-imshow(rho);
-
-%——————————————————————————————————————
-%从法线还原表面深度信息
-
-%计算深度图
-disp('开始计算深度');
-depths = calDepth(normals, objectFolder);
-disp('开始深度计算完毕');
+% %主脚本，包含了一轮重建的所有步骤
+% 
+% %——————————————————————————————————————
+% %基础文件夹指定和文件遍历数量指定
+% 
+% lightsFolder='Resources/Lights';
+% objectFolder='Resources/Apollo';
+% endIdx=6;
+% 
+% %——————————————————————————————————————
+% %计算光源信息
+% 
+% disp('开始光源处理');
+% % %用标定球计算光源
+% % lightPos=calLight(lightsFolder,endIdx);
+% %用地理信息计算光源
+% lightVec=calLightSun(lightsFolder,endIdx);
+% %保存光源
+% save([lightsFolder,'/lightPos.txt'],'lightVec','-ascii');
+% %读取光源
+% lightVec = load([lightsFolder,'/lightPos.txt'],'-ascii');
+% disp('光源处理完毕');
+% 
+% %——————————————————————————————————————
+% %还原表面法线并显示
+% 
+% %计算法线和反射率rho
+% disp('开始计算法线和反射率');
+% [normals,rho]=calNormals(objectFolder,endIdx,lightVec);
+% disp('计算法线和反射率完毕');
+% 
+% %显示法线图
+% show_surfNorm(normals, 4);
+% 
+% %RGB显示法线图
+% figure;
+% imshow(toNormalImg(normals));
+% 
+% %显示反射率图
+% figure;
+% imshow(rho);
+% 
+% %——————————————————————————————————————
+% %从法线还原表面深度信息
+% 
+% %计算深度图
+% disp('开始计算深度');
+% depths = calDepth(normals, objectFolder);
+% disp('开始深度计算完毕');
 
 %显示深度图
 disp('————');
@@ -58,6 +58,7 @@ figure;
 imshow(uint8(depths2D));
 % pause;
 figure;
-s=surfl(depths);
+%绘制时需要对深度信息进行转置才能正常显示
+s=surfl(depths');
 s.EdgeColor = 'none';
 % pause;
--- HeliometricStereo/makeMask.m
+++ HeliometricStereo/makeMask.m
@@ -1,5 +1,14 @@
 function makeMask(imageFolder,maskName)
-%制作掩码图
+%function makeMask(imageFolder,maskName)
+%将初步处理后的掩码图进一步处理，将图中的值更新为[0,255]
+%
+%imageFolder:   图片文件夹
+%maskName:      掩码图片名
+%
+%example:       makeMask('Resources/Apollo','mask.png');
+
+%——————————————————————————————————————
+%读取文件
 mask=imread([imageFolder,maskName]);
 mask=rgb2gray(mask);
 % imshow(mask);
@@ -7,6 +16,7 @@ mask=rgb2gray(mask);
 height=size(mask,1);
 width=size(mask,2);
 
+%遍历处理
 for i=1:height
     for j=1:width
         if mask(i,j)~=0
@@ -15,7 +25,8 @@ for i=1:height
     end
 end
 
+%写入文件
 imwrite(mask,['mask_',maskName]);
-figure;
-imshow(mask);
+% figure;
+% imshow(mask);
 end
